ea14634e1a3d740d5f837e3380ce03e10fd22522
wbcustc <victorwang.bc@gmail.com>
2015-09-30 14:03:56 -0400

    back-end

--- cuisine/controllers/UserController.js
+++ cuisine/controllers/UserController.js
@@ -1,27 +1,89 @@
 /**
  * Created by wangbochen on 9/23/15.
  */
+
 var models  = require('../models');
 var User = models.User;
+var jwt  = require("jsonwebtoken");
+var secret_key = "12809u9ash";
 
-exports.findUserByID = function (req,res){
+exports.ensureAuthorized = function (req, res, next) {
+	var bearerToken = req.headers["authorization"];
+	if (typeof bearerToken !== 'undefined') {
+		req.token = bearerToken;
+		next();
+	} else {
+		res.send(403);
+	}
+};
+
+exports.findUserByToken = function (req,res){
   User.findOne({
       where:{
-          id : req.params.id
+          token : req.token
       }
   }).then(function(user){
-      res.send(user);
+	  if(user) {
+		  res.json({
+			  type: true,
+			  data: user
+		  });
+	  }
   })
 };
+
+exports.authenticate = function(req,res){
+	User.findOne({
+		where: {
+			user_name: req.body.user_name,
+			password: req.body.password
+		}
+	}).then(function(user) {
+		if (user) {
+			res.json({
+				type: true,
+				data: user
+			});
+		} else {
+			res.json({
+				type: false,
+				data: "Incorrect username/password"
+			});
+		}
+	});
+};
+
 exports.createUser = function (req,res){
-    User.create({
-        user_name : req.body.user_name,
-        password : req.body.password,
-        email : req.body.email
-    }).then(function(user){
-        res.send(user);
+    User.findOne({
+        where: {
+            user_name: req.body.user_name,
+            password: req.body.password,
+            email: req.body.email
+        }
+    }).then(function(user) {
+        if (user) {
+            res.json({
+                type: false,
+                data: "User already exists!"
+            });
+        } else{
+            User.create({
+                user_name: req.body.user_name,
+                password: req.body.password,
+                email: req.body.email
+            }).then(function(user1){
+                user1.token = jwt.sign(user1,secret_key);
+                user1.save().then(function(user2){
+                    res.json({
+                        type:true,
+                        data:user2
+                    });
+                });
+            });
+        }
     });
 };
+
 exports.deleteUser = function (req,res){
     User.destroy({
         where:{
@@ -31,6 +93,7 @@ exports.deleteUser = function (req,res){
         res.send("User deleted");
     });
 };
+
 exports.updateUserEmail = function (req,res){
     User.update(
     {
--- cuisine/models/User.js
+++ cuisine/models/User.js
@@ -5,11 +5,10 @@
 
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('User', {
-        first_name: {type:DataTypes.STRING},
-        last_name: {type:DataTypes.STRING},
         user_name: {type:DataTypes.STRING,allowNull:false},
         email: {type:DataTypes.STRING,allowNull:false},
-        password: {type:DataTypes.STRING,allowNull:false}
+        password: {type:DataTypes.STRING,allowNull:false},
+        token:{type:DataTypes.STRING(500)}
     }, {
         classMethods: {
             associate: function (models) {
